20d79802a6d8a4e7606acb2c0a295233245c2e26
Karl J. Overå <k@rl.run>
2023-04-01 15:01:44 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View className="flex">
+      <BarCodeScanner
+        className="w-1/2 h-1/2 bg-amber-200"
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
+    </View>
+  );
+}
